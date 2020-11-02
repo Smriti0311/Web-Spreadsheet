f67b6f1c6490e88ae27b58316f9ed83acfa8a7fb
Smriti Ganesh Rao <srao12@CS444-srao12.pods.bu.int>
2020-11-01 23:21:40 -0500

    retrying merge of prj3 with master

--- submit/prj3-sol/src/ws-server.mjs
+++ submit/prj3-sol/src/ws-server.mjs
@@ -4,6 +4,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import {AppError} from 'cs544-ss';
+import { type } from 'os';
 
 /** Storage web service for spreadsheets.  Will report DB errors but
  *  will not make any attempt to report spreadsheet errors like bad
@@ -46,11 +47,210 @@ const STORE = 'store';
 function setupRoutes(app) {
   app.use(cors(CORS_OPTIONS));  //needed for future projects
   //@TODO add routes to handlers
+  app.use(bodyParser.json());
+  app.get(`/${BASE}/${STORE}/:id`,doGet(app));
+  app.delete(`/${BASE}/${STORE}/:ssname/:id`,doDelete(app));
+  app.delete(`/${BASE}/${STORE}/:id`, doClear(app));
+  app.patch(`/${BASE}/${STORE}/:id`, doUpdateAll(app));
+  app.patch(`/${BASE}/${STORE}/:ssname/:id`, doUpdate(app));
+  app.put(`/${BASE}/${STORE}/:id`,doReplaceAll(app));
+  app.put(`/${BASE}/${STORE}/:ssname/:id`,doReplace(app));
+
+
+ //must be last
+ app.use(do404(app));
+ app.use(doErrors(app));
+  
 }
 
 /****************************** Handlers *******************************/
 
 //@TODO
+
+// replaces particular cell of spreadsheet
+function doReplace(app){
+  return (async function(req, res){
+    try {
+      if('formula' in req.body){
+        const id = req.params.id;
+        const ssname = req.params.ssname;
+        const results = await app.locals.ssStore.delete(ssname, id);
+        const body = req.body['formula'];
+        const results1 = await app.locals.ssStore.updateCell(ssname,id, body);
+        res.status(CREATED).json(results1);  
+      }   
+      else{
+        const results3 = {'error': {
+          "code": "BAD_REQUEST",
+          "message": "request body must be a { formula }"
+        },
+        "status": 400
+            }
+            res.status(BAD_REQUEST).json(results3);
+      } 
+    }
+    catch(err){
+      const mapped = mapError(err);
+      res.status(mapped.status).json(mapped); 
+    }
+  });
+}
+
+// replaces entire spreadsheet
+function doReplaceAll(app){
+  return (async function(req, res){
+    try{
+      let flag = 1;
+      for(let b of req.body){
+        if(b.constructor !== Array){
+          flag = 0;
+          break;
+        }
+      }
+      if(flag === 1){
+        const id = req.params.id;
+        const results = await app.locals.ssStore.clear(id);
+        const body = req.body;
+        let results1 = {};
+        for(let b of body){
+          results1 = await app.locals.ssStore.updateCell(id, b[0], b[1].toString());
+        }
+        res.status(CREATED).json(results1);
+      }
+      else{
+        const results3 = {'error': {
+          "code": "BAD_REQUEST",
+          "message": "request body must be a list of cellid, formula pairs"
+        },
+        "status": 400
+            }
+            res.status(BAD_REQUEST).json(results3);
+      }
+    }
+    catch (err){
+      const mapped = mapError(err);
+      res.status(mapped.status).json(mapped); 
+    }
+  });
+}
+
+
+// updates particular spreadsheet cell
+function doUpdate(app){
+  return (async function(req, res){
+    try {
+      if('formula' in req.body){
+      const id = req.params.id;
+      const body = req.body['formula'];
+      const ssname = req.params.ssname;
+      const results = await app.locals.ssStore.updateCell(ssname, id, body.toString());
+      res.status(NO_CONTENT).json(results);
+      }
+      else{
+        const results3 = {'error': {
+          "code": "BAD_REQUEST",
+          "message": "request body must be a { formula }"
+        },
+        "status": 400
+            }
+            res.status(BAD_REQUEST).json(results3);
+      }  
+    }
+    catch (err){
+      const mapped = mapError(err);
+      res.status(mapped.status).json(mapped); 
+    }
+  });
+}
+
+// updates entire spreadsheet
+function doUpdateAll(app){
+  return (async function(req, res){
+    try {
+      let flag = 1;
+      for(let b of req.body){
+        if(b.constructor !== Array){
+          flag = 0;
+          break;
+        }
+      }
+      if(flag === 1){
+        const id = req.params.id;
+        const body = req.body;
+        let results = {}
+        for(let b of body){
+          results = await app.locals.ssStore.updateCell(id, b[0], b[1]);
+        }
+        res.status(NO_CONTENT).json(results);
+      }
+      else{
+        const results3 = {'error': {
+          "code": "BAD_REQUEST",
+          "message": "request body must be a list of cellid, formula pairs"
+        },
+        "status": 400
+            }
+            res.status(BAD_REQUEST).json(results3);
+      }   
+    }
+    catch (err){
+      const mapped = mapError(err);
+      res.status(mapped.status).json(mapped);
+    }
+  });
+}
+
+// retrieves all spreadsheet data 
+function doGet(app){
+ return (async function(req, res){
+ try {
+   const testss = req.params.id;
+   const results = await app.locals.ssStore.readFormulas(testss);
+   res.json(results);
+ }
+ catch(err) {
+   const mapped = mapError(err);
+   res.status(mapped.status).json(mapped);
+    }
+  });
+ }
+
+// deletes content of a particular cell
+ function doDelete(app){
+   return (async function(req, res){
+     try{
+       const id = req.params.id;
+       const ssname = req.params.ssname;
+       const results = await app.locals.ssStore.delete(ssname, id);
+       res.status(204).json(results);
+     }
+     catch(err){
+       const mapped = mapError(err);
+       res.status(mapped.status).json(mapped);
+     }
+   });
+ }
+ 
+
+// Deletes contents of entire spreadsheet
+ function doClear(app){
+   return (async function(req, res){
+     try{
+       const id = req.params.id;
+       const results = await app.locals.ssStore.clear(id);
+       res.status(204).json(results);
+     }
+     catch(err){
+       const mapped = mapError(err);
+       res.status(mapped.status).json(mapped);
+     }
+   });
+ }
+
+
+ 
+
+
 
 /** Default handler for when there is no route for a particular method
  *  and path.
@@ -86,7 +286,8 @@ function doErrors(app) {
 /*************************** Mapping Errors ****************************/
 
 const ERROR_MAP = {
-}
+
+};
 
 /** Map domain/internal errors into suitable HTTP errors.  Return'd
  *  object will have a "status" property corresponding to HTTP status
